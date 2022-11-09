8c15abe1680792e9ca9a526a4d5a1212867a8edf
Blanche Haddad <blancherachel@gmail.com>
2022-11-08 20:20:07 -0700

    feat: Save some notes in main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+ // how does the token get into the possible positions? 
+
+        // IN MAIN JS IF IT"S THIS PLAYERS TURN< SNET THE INNTERTEXT TO THAT PLAYER'S TOKEN. 
+        // BOARD SHOILD BE DISABLED UNTIL ITS RESET--- add diaabled to the class after the game is done.
+// when the game ends, wait a few seconds, and reset the board. 
